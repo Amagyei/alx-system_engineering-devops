1762889ff7ac96de311a72051cd564bead881181
amagyei <kwame.na@hotmail.com>
2024-07-03 18:04:06 -0700

    task 0

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# install flask with puppet lint
+
+package  {'python3-pip':
+  ensure => installed,
+}
+
+exec {'install_flask':
+  command => '/usr/bin/pip3 insall flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
+} 
