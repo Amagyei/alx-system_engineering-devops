5be411f368830344ca9b8131acd58cc22ad7ecd9
amagyei <kwame.na@hotmail.com>
2024-07-03 18:10:38 -0700

    task 0

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -8,4 +8,4 @@ exec {'install_flask':
   command => '/usr/bin/pip3 insall flask==2.1.0',
   unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
   require => Package['python3-pip'],
-} 
+}
